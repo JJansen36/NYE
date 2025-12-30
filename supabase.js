122797265f774260de137c52a7fda65ad7234afa
JJansen36 <97358685+JJansen36@users.noreply.github.com>
2025-12-30 19:03:20 +0100

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,10 +1,9 @@
-// ================================
-// Supabase client init
-// Vul je eigen project URL + anon key in
-// ================================
-const SUPABASE_URL = "https://wjvleduvvjjseunakmcb.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdmxlZHV2dmpqc2V1bmFrbWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NDU0NzYsImV4cCI6MjA4MjUyMTQ3Nn0.9GzBjksztdM4SFw_BcAVKl3gwEzBN0jqNhztQ9m9uj0";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: { persistSession: false } // simpel: geen login nodig
-});
+export const sb = createClient(
+    "https://wjvleduvvjjseunakmcb.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdmxlZHV2dmpqc2V1bmFrbWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NDU0NzYsImV4cCI6MjA4MjUyMTQ3Nn0.9GzBjksztdM4SFw_BcAVKl3gwEzBN0jqNhztQ9m9uj0",
+  {
+    auth: { persistSession: false }
+  }
+);
