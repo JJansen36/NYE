9a74c20503c0451adba69c7e5f8edc6cbd619d86
JJansen36 <97358685+JJansen36@users.noreply.github.com>
2025-12-30 19:49:52 +0100

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const SUPABASE_URL = "https://wjvleduvvjjseunakmcb.supabase.co";
-const SUPABASE_ANON_KEY = "";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqdmxlZHV2dmpqc2V1bmFrbWNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5NDU0NzYsImV4cCI6MjA4MjUyMTQ3Nn0.9GzBjksztdM4SFw_BcAVKl3gwEzBN0jqNhztQ9m9uj0";
 
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: false }
